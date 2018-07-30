348e3edf91cf044f2c2484ce246c3402ef984707
PunitGr <groove679@gmail.com>
2018-07-31 04:45:47 +0530

    Fix flow errors

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -6,29 +6,35 @@ import differenceInHours from 'date-fns/difference_in_hours';
 import differenceInMinutes from 'date-fns/difference_in_minutes';
 import differenceInSeconds from 'date-fns/difference_in_seconds';
 
-export const getTimerState = state => (state.timerState ? 'Stop' : 'Play');
-export const getStartTime = state => state.startTime;
-export const getStopTime = state => state.stopTime;
-export const getCurrentTime = state => state.currentTime;
+type State = {
+  timerState: ?string,
+  startTime: ?string,
+  stopTime: ?string,
+  currentTime: ?string,
+};
+
+export const getTimerState = (state: State) => (state.timerState ? 'Stop' : 'Play');
+export const getStartTime = (state: State) => state.startTime;
+export const getStopTime = (state: State) => state.stopTime;
+export const getCurrentTime = (state: State) => state.currentTime;
 
 export const getWorkedTimeNormalized = createSelector(
   [getTimerState, getStartTime, getStopTime, getCurrentTime],
   (timerState, startTime, stopTime, currentTime) => {
-    let relativeTime = '00';
+    let relativeTime = 0;
     if (isEqual(startTime, currentTime)) {
-      relativeTime = '00';
+      relativeTime = 0;
     } else if (timerState === 'Stop' && startTime && currentTime) {
       relativeTime = differenceInWordsStrict(startTime, currentTime);
     } else if (startTime && stopTime) {
       relativeTime = differenceInWordsStrict(startTime, stopTime);
     }
 
-    relativeTime =
-      relativeTime !== '00'
-        ? `${(parseInt(relativeTime.split(' ')[0], 10) < 10 ? '0' : '') +
-            relativeTime.split(' ')[0] +
-            relativeTime.split(' ')[1].slice()[0]}`
-        : relativeTime;
+    relativeTime = !relativeTime
+      ? '00'
+      : `${(parseInt(relativeTime.split(' ')[0], 10) < 10 ? '0' : '') +
+          relativeTime.split(' ')[0] +
+          relativeTime.split(' ')[1].slice()[0]}`;
 
     return relativeTime;
   },
@@ -37,46 +43,46 @@ export const getWorkedTimeNormalized = createSelector(
 export const getHours = createSelector(
   [getTimerState, getStartTime, getStopTime, getCurrentTime],
   (timerState, startTime, stopTime, currentTime) => {
-    let hours = '00';
+    let hours = 0;
     if (isEqual(startTime, currentTime)) {
-      hours = '00';
+      hours = 0;
     } else if (timerState === 'Stop' && startTime && currentTime) {
       hours = differenceInHours(currentTime, startTime);
     } else if (startTime && stopTime) {
       hours = differenceInHours(stopTime, startTime);
     }
-    return hours !== '00' ? `${hours < 10 ? '0' : ''}${hours}` : hours;
+    return !hours ? '00' : `${hours < 10 ? '0' : ''}${hours}`;
   },
 );
 
 export const getMinutes = createSelector(
   [getTimerState, getStartTime, getStopTime, getCurrentTime],
   (timerState, startTime, stopTime, currentTime) => {
-    let minutes = '00';
+    let minutes = 0;
     if (isEqual(startTime, currentTime)) {
-      minutes = '00';
+      minutes = 0;
     } else if (timerState === 'Stop' && startTime && currentTime) {
       minutes = differenceInMinutes(currentTime, startTime);
     } else if (startTime && stopTime) {
       minutes = differenceInMinutes(stopTime, startTime);
     }
     minutes %= 60;
-    return minutes !== '00' ? `${minutes < 10 ? '0' : ''}${minutes}` : minutes;
+    return !minutes ? '00' : `${minutes < 10 ? '0' : ''}${minutes}`;
   },
 );
 
 export const getSeconds = createSelector(
   [getTimerState, getStartTime, getStopTime, getCurrentTime],
   (timerState, startTime, stopTime, currentTime) => {
-    let seconds = '00';
+    let seconds = 0;
     if (isEqual(startTime, currentTime)) {
-      seconds = '00';
+      seconds = 0;
     } else if (timerState === 'Stop' && startTime && currentTime) {
       seconds = differenceInSeconds(currentTime, startTime);
     } else if (startTime && stopTime) {
       seconds = differenceInSeconds(stopTime, startTime);
     }
     seconds %= 60;
-    return seconds !== '00' ? `${seconds < 10 ? '0' : ''}${seconds}` : seconds;
+    return !seconds ? '00' : `${seconds < 10 ? '0' : ''}${seconds}`;
   },
 );
