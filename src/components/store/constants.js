1734dbe0b2f36958d9f1c8968299f3d8313c7a61
PunitGr <groove679@gmail.com>
2018-07-31 04:18:11 +0530

    code cleanup

--- src/components/store/constants.js
+++
@@ -1,6 +0,0 @@
-// @flow
-export const RESET_TIMER: string = 'RESET_TIMER';
-export const TOGGLE_TIMER_STATE: string = 'TOGGLE_TIMER_STATE';
-export const SET_START_TIME: string = 'SET_START_TIME';
-export const SET_STOP_TIME: string = 'SET_STOP_TIME';
-export const SET_CURRENT_TIME: string = 'SET_CURRENT_TIME';
